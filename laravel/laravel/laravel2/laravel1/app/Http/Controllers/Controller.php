7732df7782d3f18fef80fafd513682323ceb49f2
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-08-28 23:16:01 +0700

    restructure app

---
+++ laravel/laravel/laravel2/laravel1/app/Http/Controllers/Controller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use App\Models\Education;
+use App\Models\Certificate;
+use Illuminate\Support\Facades\View;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    public function index()
+    {
+        // Ambil semua data dari tabel products
+        $projects = Project::all();
+
+        // Ambil semua data dari tabel products
+        $certificates = Certificate::all();
+
+        // Ambil semua data dari tabel products
+        $educations = Education::all();
+        
+        // Kirim data ke view
+        return view('home', compact('projects', 'certificates', 'educations'));
+    }
+
+    public function show($slug)
+    {
+        // Cari proyek berdasarkan slug
+        $project = Project::where('slug', $slug)->firstOrFail();
+
+        // Nama view berdasarkan category
+        $viewName = strtolower($project->category);
+
+        // Tampilkan view dengan data proyek
+        return View::make($viewName, ['project' => $project]);
+    }
+}
---
+++ laravel/laravel/laravel2/laravel1/resources/views/components/navbar.blade.php
@@ -0,0 +1,36 @@
+    {{-- toggle untuk kembali ke atas --}}
+    <a href="#top" class="back-to-top" id="backToTop">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
+      </svg>
+    </a>
+    {{-- akhir toggle untuk kembali ke atas --}}
+    
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent shadow-sm fixed-top">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto mx-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/#home">Hello</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/#projects">Projects</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/#certificate">Certification</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#education">Education</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
