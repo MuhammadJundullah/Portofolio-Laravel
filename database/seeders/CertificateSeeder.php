fb46219119d21daf45257d2be2a32e14e53d2eb9
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-09-01 18:03:54 +0700

    add certificate

--- database/seeders/CertificateSeeder.php
+++ database/seeders/CertificateSeeder.php
@@ -20,11 +20,13 @@ class CertificateSeeder extends Seeder
     {
         // Data yang ingin diisi
         $certificates = [
+            ['name' => 'ds_pijarmahir.png', 'desc' => 'Mahir Data Science dari nol.', 'date' => '28 Agustus 2024'],
             ['name' => 'dqlab_py.png', 'desc' => 'Introduction to Data Science with Python.', 'date' => '12 August 2023'],
+            ['name' => 'dqlab_fundamental_py.png', 'desc' => 'Python Fundamental for Data Science.', 'date' => '1 September 2024'],
             ['name' => 'dqlab_r.png', 'desc' => 'Introduction to Data Science with R.', 'date' => '12 May 2023'],
+            ['name' => 'dqlab_sql.png', 'desc' => 'Fundamental SQL Using SELECT Statement.', 'date' => '1 September 2024'],
             ['name' => 'pmm.png', 'desc' => 'Pertukaran Mahasiswa Merdeka Batch 3.', 'date' => '5 April 2024'],
             ['name' => 'inpro.png', 'desc' => '3rd Place in Product Innovation Competition.', 'date' => '2019'],
-            ['name' => 'ds_pijarmahir.png', 'desc' => 'Mahir Data Science dari nol.', 'date' => '28 Agustus 2024'],
             // Tambahkan data lainnya sesuai kebutuhan
         ];
 
